3da223a48a786189a924759f8ccdfb3fdc78e454
Dang Quang <quangthat97@gmail.com>
2023-10-19 08:18:46 +0700

    complete register page functionally

--- client/src/pages/register.jsx
+++ client/src/pages/register.jsx
@@ -1,34 +1,77 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Register() {
+	const [formData, setFormData] = useState({});
+	const [error, setError] = useState(null);
+	const [isLoading, setIsLoading] = useState(false);
+
+	const handleChange = (e) => {
+		setFormData({
+			...formData,
+			[e.target.id]: e.target.value,
+		});
+	};
+
+	const handlerSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			setIsLoading(true);
+			const res = await fetch('/api/auth/register', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify(formData),
+			});
+			const data = await res.json();
+
+			if (data.success === false) {
+				setError(data.message);
+				setIsLoading(false);
+				return;
+			}
+			setIsLoading(false);
+			setError(null);
+		} catch (error) {
+			setIsLoading(false);
+			setError(error.message);
+		}
+	};
+
 	return (
 		<div className='max-w-lg p-3 mx-auto'>
 			<h1 className='text-3xl font-semibold text-center my-7'>
 				Register
 			</h1>
-			<form className='flex flex-col gap-4'>
+			<form onSubmit={handlerSubmit} className='flex flex-col gap-4'>
 				<input
 					type='text'
 					placeholder='Username...'
 					className='p-3 border rounded-lg'
 					id='username'
+					onChange={handleChange}
 				/>
 				<input
 					type='email'
 					placeholder='Email...'
 					className='p-3 border rounded-lg'
 					id='email'
+					onChange={handleChange}
 				/>
 				<input
 					type='password'
 					placeholder='Password...'
 					className='p-3 border rounded-lg'
 					id='password'
+					onChange={handleChange}
 				/>
-				<button className='p-3 text-white uppercase bg-blue-500 rounded-lg hover:opacity-95 disabled:opacity-75'>
-					Register
+				<button
+					disabled={isLoading}
+					className='p-3 text-white uppercase bg-blue-500 rounded-lg hover:opacity-95 disabled:opacity-75'
+				>
+					{isLoading ? 'Loading...' : 'Register'}
 				</button>
 			</form>
+			{error && <p className='mt-5 text-red-500'>{error}</p>}
 			<div className='flex gap-2 mt-5'>
 				<p>Have an account?</p>
 				<Link to='/login'>
