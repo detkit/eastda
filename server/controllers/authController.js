4f43e132e4eb981aa572cd2a3b744dc454f8146c
Dang Quang <quangthat97@gmail.com>
2023-10-19 12:17:30 +0700

    create login route server auth

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,5 @@
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 import User from '../models/userModel.js';
 
 export const register = async (req, res, next) => {
@@ -8,6 +9,30 @@ export const register = async (req, res, next) => {
 	try {
 		await newUser.save();
 		res.status(201).json('User successfully registered');
+	} catch (error) {
+		next(error);
+	}
+};
+
+export const login = async (req, res, next) => {
+	const { email, password } = req.body;
+
+	try {
+		const validUser = await User.findOne({ email: email });
+
+		if (!validUser) return next(errorHandler(404, 'User not found'));
+		const validPassword = bcryptjs.compareSync(
+			password,
+			validUser.password
+		);
+		if (!validPassword) {
+			return next(errorHandler(404, 'Wrong credentials!'));
+		}
+		const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+		const { password: hashedPassword, ...userInfo } = validUser._doc;
+		res.cookie('access_token', token, { httpOnly: true })
+			.status(200)
+			.json(userInfo);
 	} catch (error) {
 		next(error);
 	}
