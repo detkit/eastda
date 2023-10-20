69b862ea273a4ed86363aaa4e754a215bbe6de39
Dang Quang <quangthat97@gmail.com>
2023-10-20 22:14:47 +0700

    listing single page show

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,4 +1,12 @@
 import { useEffect, useState } from 'react';
+import {
+	FaBath,
+	FaBed,
+	FaChair,
+	FaMapMarkerAlt,
+	FaParking,
+	FaShare,
+} from 'react-icons/fa';
 import { useParams } from 'react-router-dom';
 import SwiperCore from 'swiper';
 import 'swiper/css/bundle';
@@ -10,6 +18,7 @@ export default function Listing() {
 	const [listing, setListing] = useState(null);
 	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState(false);
+	const [copied, setCopied] = useState(false);
 	const params = useParams();
 
 	useEffect(() => {
@@ -59,6 +68,84 @@ export default function Listing() {
 							</SwiperSlide>
 						))}
 					</Swiper>
+					<div className='fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center bg-slate-100 items-center cursor-pointer'>
+						<FaShare
+							className='text-slate-500'
+							onClick={() => {
+								navigator.clipboard.writeText(
+									window.location.href
+								);
+								setCopied(true);
+								setTimeout(() => {
+									setCopied(false);
+								}, 2000);
+							}}
+						/>
+					</div>
+					{copied && (
+						<p className='fixed top-[23%] right-[15%] z-10 rounded-md bg-slate-100 p-2'>
+							Copy Link!
+						</p>
+					)}
+					<div className='flex flex-col max-w-4xl gap-4 p-3 mx-auto my-7'>
+						<p className='text-2xl font-semibold'>
+							{listing.name} - $
+							{listing.offer
+								? listing.discountPrice.toLocaleString('en-US')
+								: listing.regularPrice.toLocaleString('en-US')}
+							{listing.type === 'rent' && ' / month'}
+						</p>
+						<p className='flex items-center gap-2 my-2 mt-6 text-sm text-slate-600'>
+							<FaMapMarkerAlt className='text-sky-600' />
+							{listing.address}
+						</p>
+						<div className='flex gap-4'>
+							<p className='bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
+								{listing.type === 'rent'
+									? 'For Rent'
+									: 'For Sale'}
+							</p>
+							{listing.offer && (
+								<p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
+									$
+									{+listing.regularPrice -
+										+listing.discountPrice}
+								</p>
+							)}
+						</div>
+						<p className='text-slate-800'>
+							<span className='font-semibold text-black'>
+								Description -{' '}
+							</span>
+							{listing.description}
+						</p>
+						<ul className='flex flex-wrap items-center gap-6 text-sm font-semibold text-green-900'>
+							<li className='flex items-center gap-1 whitespace-nowrap'>
+								<FaBed className='text-lg' />
+								{listing.bedrooms > 1
+									? `${listing.bedrooms} beds`
+									: `${listing.bedrooms} bed`}
+							</li>
+							<li className='flex items-center gap-1 whitespace-nowrap'>
+								<FaBath className='text-lg' />
+								{listing.bathrooms > 1
+									? `${listing.bathrooms} baths`
+									: `${listing.bathrooms} bath`}
+							</li>
+							<li className='flex items-center gap-1 whitespace-nowrap'>
+								<FaParking className='text-lg' />
+								{listing.parking
+									? 'Parking spot'
+									: 'No Parking'}
+							</li>
+							<li className='flex items-center gap-1 whitespace-nowrap'>
+								<FaChair className='text-lg' />
+								{listing.furnished
+									? 'Furnished'
+									: 'No Furnished'}
+							</li>
+						</ul>
+					</div>
 				</div>
 			)}
 		</main>
